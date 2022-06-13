c5aebf14eaf525c7c40d2e65c97141594588aff6
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-06-13 11:22:03 +0800

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,115 @@
 #!/bin/sh
-rm -rf /etc/trojan-go/config.json
-cat << EOF > /etc/trojan-go/config.json
+rm -rf /etc/xray/config.json
+cat << EOF > /etc/xray/config.json
+{
+  "inbounds": [
     {
-      "run_type": "server",
-    "local_addr": "0.0.0.0",
-    "local_port": 443,
-    "remote_addr": "127.0.0.1",
-    "remote_port": 80,
-    "password": [
-        "$UUID"
-    ],
-"websocket": {
-    "enabled": true,
-    "hostname": "www.your-awesome-domain-name.com"
-    },
-    "router":{
-        "enabled": true,
-        "block": [
-            "geoip:private"
+      "port": $PORT,
+      "protocol": "vless",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
         ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      },
+      "sniffing": {
+                "enabled": true,
+                "destOverride": [
+                     "http",
+                     "tls"
+                ]
+            }
+},
+    {
+      "port": $PORT,
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ] 
+      },
+      "streamSettings": {
+        "network": "ws"
+      },
+      "sniffing": {
+                "enabled": true,
+                "destOverride": [
+                     "http",
+                     "tls"
+                ]
+            }
+},
+    {
+      "port": $PORT,
+      "protocol": "trojan",
+      "settings": {
+        "clients": [
+          {
+            "password": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      },
+      "sniffing": {
+                "enabled": true,
+                "destOverride": [
+                     "http",
+                     "tls"
+                ]
+            }
+    }
+  ],
+  "routing": {
+        "domainStrategy": "IPIfNonMatch",
+        "domainMatcher": "mph",
+        "rules": [
+           {
+              "type": "field",
+              "protocol": [
+                 "bittorrent"
+              ],
+              "domains": [
+                  "geosite:cn",
+                  "geosite:category-ads-all"
+              ],
+              "outboundTag": "blocked"
+           }
+        ]
+    },
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {
+               "domainStrategy": "UseIPv4",
+               "userLevel": 0
+            }
+        },
+        {
+            "protocol": "blackhole",
+            "tag": "blocked"
+        }
+    ],
+    "dns": {
+        "servers": [
+            {
+                "address": "https+local://dns.google/dns-query",
+                "address": "https+local://cloudflare-dns.com/dns-query",
+                "skipFallback": true
+            }
+        ],
+        "queryStrategy": "UseIPv4",
+        "disableCache": true,
+        "disableFallbackIfMatch": true
     }
 }
 EOF
-xray -c /etc/trojan-go/config.json
+xray -c /etc/xray/config.json
